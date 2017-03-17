c7d4cea2d8bf4c366f8f352988494e77df86500e
Pablo Manrubia <pmanrubia@gmail.com>
2017-03-17 21:27:02 +0000

    Add endpoint to create a Group
    Add endpoint `POST /groups`. The endpoint returns a JSON including the
    details of the Group that has been created, or a JSON including any
    validation errors raised during the creation.
    
    At this moment we are not including the parameters:
    
    - parent_group_id
    - associations with content_items

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,22 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+
+  def group_params
+    params.require(:group).permit(:slug, :name, :parent_group_slug, :group_type)
+  end
 end
