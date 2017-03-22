06aff6fd3035ab1fb5fa85438f36503c40206446
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-22 12:09:17 +0000

    Use publishing api for content items
    Replace calls to content_store and search_api.
    Instead content is retrieved by document_type from the publishing-api.
    The document types are configured in a .yml file for now.
    This will change once there is an endpoint to retrieve a live list.
    Org and Taxon associations use the pub-api's expanded_links endpoint.
    Calls to get/import content by organisation have been removed.

--- app/models/importers/all_content_items.rb
+++ app/models/importers/all_content_items.rb
@@ -1,10 +1,20 @@
 module Importers
   class AllContentItems
-    def run
-      content_item_importer = ContentItemsByOrganisation.new
+    attr_accessor :metric_builder, :content_items_service
 
-      Organisation.find_each do |organisation|
-        content_item_importer.run(organisation.slug)
+    def initialize
+      @metric_builder = MetricBuilder.new
+      @content_items_service = ContentItemsService.new
+    end
+
+    def run
+      document_types = Rails.configuration.document_types
+
+      document_types.each do |type|
+        content_items_service.find_each(type) do |content_item_attributes|
+          metrics = metric_builder.run_all(content_item_attributes)
+          ContentItem.create_or_update!(metrics.merge(content_item_attributes))
+        end
       end
     end
   end
