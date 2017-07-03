77bfc820978f179efec3abb74cb4dc8d72e99c72
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-03 10:54:11 +0100

    --wip-- [skip ci]

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,18 +1,33 @@
 class ContentItemsController < ApplicationController
-  helper_method :filter_params
+  helper_method :filter_params, :primary_org_only?, :org_link_type
 
   def index
     @search = Search.new
-    @search.filter_by(link_type: Search::FILTERABLE_LINK_TYPES, target_ids: params[:organisations]) if params[:organisations].present?
+    filter_by_organisation!(@search)
     @search.page = params[:page]
     @search.execute
 
-    @metrics = MetricBuilder.new.run_collection(@search.content_items)
+    @metrics = MetricBuilder.new.run_collection(@search.unpaginated)
 
     @content_items = @search.content_items.decorate
   end
 
   def show
     @content_item = ContentItem.find(params[:id]).decorate
+  end
+
+  private
+
+  def filter_by_organisation!(search)
+    content_id = params[:organisations]
+    search.filter_by(link_type: org_link_type, target_ids: content_id) if content_id.present?
+  end
+
+  def org_link_type
+    primary_org_only? ? Link::PRIMARY_ORG : Link::ALL_ORGS
+  end
+
+  def primary_org_only?
+    params[:primary].blank? || params[:primary] == "true"
   end
 end
